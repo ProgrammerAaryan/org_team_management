097f9f1e7c57cc78e8b041cc78924395209977fb
Aaryan <aaryankumar57834@gmail.com>
2024-12-30 00:20:01 +0530

    Add whitenoise for static file handling in production

--- org_team_management/settings.py
+++ org_team_management/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    # other middleware
+    
 
 
 ]
